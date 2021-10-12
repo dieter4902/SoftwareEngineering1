4a56ff08ecdc5fd3d575450ef930c77ec1f1e591
Dieter123 <adrianjost1@gmx.de>
2021-10-12 16:57:46 +0200

    added javafx

--- setup.javafx/src/application/Application.java
+++ setup.javafx/src/application/Application.java
@@ -1,31 +1,39 @@
 package application;
 
+import javafx.application.Platform;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
+
 public class Application extends javafx.application.Application {
 
-	@Override
-	public void start(Stage stage) {
-		try {
-			stage.setTitle("Hello JavaFX");
-			VBox root = new VBox();
-			Scene scene = new Scene(root, 400, 400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			stage.setScene(scene);
-			Button btn = new Button("Hello World!");
-			btn.setOnAction(e -> System.out.println("Hello World!"));
-			root.getChildren().add(btn);
-			stage.show();
+    @Override
+    public void start(Stage stage) {
+        try {
+            stage.setTitle("Hello JavaFX");
+            VBox root = new VBox();
+            Scene scene = new Scene(root, 400, 400);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            stage.setScene(scene);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            Button btn = new Button("Hello World!");
+            btn.setOnAction(e -> System.out.println("Hello World!"));
+            root.getChildren().add(btn);
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+            Button exitBtn = new Button("EXIT");
+            exitBtn.setOnAction(e -> Platform.exit());
+            root.getChildren().add(exitBtn);
+
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
