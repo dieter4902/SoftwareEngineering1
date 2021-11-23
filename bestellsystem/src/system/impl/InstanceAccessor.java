a814453ec9afd36dc28dd3a2ee0ad9af6d7cc0b6
Dieter123 <adrianjost1@gmx.de>
2021-11-23 13:15:27 +0100

    d2 anfang

--- bestellsystem/src/system/impl/InstanceAccessor.java
+++ bestellsystem/src/system/impl/InstanceAccessor.java
@@ -7,11 +7,11 @@ import system.RTE;
  * Only public class in this package to provide access to the singleton
  * instance of the Runtime Environment (RTE) that implements the public
  * RTE interface.
- * 
+ *
  * The RTE interface offers public interfaces of all system components
  * through their public interfaces. Implementations are hidden as local
  * (none-public) classes in this package.
- * 
+ *
  * InstanceAccessor implements the singleton pattern for the RTE_Impl()
  * instance.
  *
@@ -27,7 +27,7 @@ public class InstanceAccessor {
 
 	/**
 	 * Static access method to RTE singleton instance (singleton pattern).
-	 * 
+	 *
 	 * @return reference to RTE singleton instance.
 	 */
 
@@ -36,7 +36,7 @@ public class InstanceAccessor {
 		 * lazy instance creation (only when getInstance() is called)
 		 */
 		if( rte_instance == null ) {
-			//rte_instance = new RTE_Impl();
+			rte_instance = new RTE_Impl();
 		}
 		return rte_instance;
 	}
